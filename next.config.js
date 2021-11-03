08faf98884cf0d754369f45002abc13f44f2f43e
Daniel Chiang Guerrero <chiang_guerrero_daniel@hotmail.com>
2021-11-03 18:12:45 -0500

    fresh next js app with support for github pages using a exported static version of the app

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const debug = process.env.NODE_ENV !== "production";
+const prod = process.env.NODE_ENV === "production";
+
 module.exports = {
+  basePath: prod ? "/nextjs-portfolio" : "",
   reactStrictMode: true,
-}
+  assetPrefix: !debug ? "./" : "",
+};
