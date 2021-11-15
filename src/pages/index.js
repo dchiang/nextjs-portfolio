76d76c955d418883dac64f0e6247a2d21800bc0f
Daniel Chiang Guerrero <chiang_guerrero_daniel@hotmail.com>
2021-11-15 16:57:49 -0500

    directly define SEO head inside index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,16 @@ import styles from "../../styles/Home.module.css";
 import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 import Header from "../components/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Header
-        title="Daniel Chiang"
-        description="Portfolio developed using next js"
-      />
+      <Head>
+        <title>Daniel Chiang Guerrero Portfolio</title>
+        <meta name="description" content="Portfolio developed using next js" />
+        <link rel="icon" href="../pictures/favicon.ico" />
+      </Head>
       <Layout>
         <main className={styles.main}>
           <h1 className={styles.title}>
@@ -52,7 +54,6 @@ export default function Home() {
           </div>
         </main>
       </Layout>
-
       <Footer />
     </div>
   );
