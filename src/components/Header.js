0850a1868c77a78e452581266b9cf6cbb2d47308
Daniel Chiang Guerrero <chiang_guerrero_daniel@hotmail.com>
2021-11-16 02:18:28 -0500

    replaced level layout with columns inside hero header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,27 @@ const Header = (props) => {
       </section>
       <section className="hero is-medium is-link">
         <div className="hero-body">
-          <div className="level">
-            <div className="level-item has-text-centered">
+          <div className="columns">
+            <div className="column is-two-fifths has-text-right has-text-centered-mobile">
               <img
                 src="../pictures/Daniel.jpg"
-                alt="https://www.linkedin.com/in/daniel-chiang-guerrero/"
+                alt="Daniel Chiang Guerrero"
                 width="200"
                 height="200"
               />
             </div>
-            <div className="level-item">
+            <div className="column">
               <p className="title">Daniel Chiang Guerrero</p>
-              <p className="subtitle">portfolio</p>
+              <p className="subtitle mb-3">Full Stack Developer</p>
+              <p className="block">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
             </div>
           </div>
         </div>
