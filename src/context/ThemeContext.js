21081146ff9986c1713ba9038926a9ece1753b2a
Daniel Chiang Guerrero <chiang_guerrero_daniel@hotmail.com>
2021-11-16 17:40:44 -0500

    removed unnecesary log to console used for debuggin

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -5,7 +5,6 @@ const ThemeContext = createContext(false);
 const ThemeProvider = (props) => {
   const [toggle, setToggle] = useState(false);
   const toggleFunction = () => {
-    console.log(!toggle);
     setToggle(!toggle);
   };
   return (
