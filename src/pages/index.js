aafa0a0d609a023dfe7cff5f831ed059a2a96d59
Daniel Chiang Guerrero <chiang_guerrero_daniel@hotmail.com>
2021-11-16 01:06:26 -0500

    removed initial content and styles generated by create-next-app

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,55 +6,14 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <section>
       <Head>
         <title>Daniel Chiang Guerrero Portfolio</title>
         <meta name="description" content="Portfolio developed using next js" />
         <link rel="icon" href="../pictures/favicon.ico" />
       </Head>
-      <Layout>
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by editing{" "}
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h2>Documentation &rarr;</h2>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h2>Learn &rarr;</h2>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/master/examples"
-              className={styles.card}
-            >
-              <h2>Examples &rarr;</h2>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h2>Deploy &rarr;</h2>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
-          </div>
-        </main>
-      </Layout>
+      <Layout></Layout>
       <Footer />
-    </div>
+    </section>
   );
 }
