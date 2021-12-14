07de8783f509cbbf5dc54de75dde85c5bb240076
Daniel Chiang Guerrero <chiang_guerrero_daniel@hotmail.com>
2021-12-14 13:40:15 -0500

    Updates

--- _next/static/o9xZFkzOfwJI6H-g3xX4H/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e36e9ca00e00d53e.css","static/chunks/pages/index-75dbc5bb25c75c3d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
