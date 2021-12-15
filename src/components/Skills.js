47756c536f36ec496c5d001733995c91322dc483
Daniel Chiang Guerrero <chiang_guerrero_daniel@hotmail.com>
2021-12-15 04:53:36 -0500

    ThemeContext attached to component to support toggle between themes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,11 @@
 import Carousel from "./carousel/Carousel";
 import LanguagesRadar from "./LanguagesRadar";
 import LanguageBars from "./LanguageBars";
+import { ThemeContext } from "../context/ThemeContext";
+import { useContext } from "react";
 
 const Skills = (props) => {
+  const { toggle } = useContext(ThemeContext);
   const percentage = (amountUsed) => {
     const percentage = (100 * amountUsed) / props.languagesUsed.totalSize;
     return percentage.toFixed(2);
@@ -17,16 +20,23 @@ const Skills = (props) => {
   const style = { height: "25em", width: "20em" };
 
   return (
-    <div style={{ paddingLeft: "2rem" }}>
-      <label className="title is-4 has-text-black-ter">Skills</label>
-      <div>
-        <Carousel style={style} darkTheme={false}>
-          <div>
-            <LanguageBars data={languagesUsed} darkTheme={false} />
-          </div>
-          <LanguagesRadar data={languagesUsed} darkTheme={false} />
-        </Carousel>
-      </div>
+    <div
+      className={`${props.className} has-border-1-${toggle ? "dark" : "light"}`}
+      style={props.style}
+    >
+      <label
+        className={`title is-4 pl-2 has-text-${
+          toggle ? "black-ter" : "link-dark"
+        }`}
+      >
+        Skills
+      </label>
+      <Carousel style={style} darkTheme={toggle}>
+        <div>
+          <LanguageBars data={languagesUsed} darkTheme={toggle} />
+        </div>
+        <LanguagesRadar data={languagesUsed} darkTheme={toggle} />
+      </Carousel>
     </div>
   );
 };
