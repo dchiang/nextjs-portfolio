c573cb9498e32a50c31bcbc07700938a9d6c782e
Daniel Chiang Guerrero <chiang_guerrero_daniel@hotmail.com>
2021-12-15 04:56:36 -0500

    skill component added into index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import styles from "../../styles/Home.module.css";
+import Head from "next/head";
 import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 import Header from "../components/Header";
-import Head from "next/head";
+import Skills from "../components/Skills";
+import { PortfolioContext } from "../context/PortfolioContext";
+import { useContext } from "react";
 
 export default function Home() {
+  const { portfolio } = useContext(PortfolioContext);
   return (
     <section>
       <Head>
@@ -14,6 +18,12 @@ export default function Home() {
       </Head>
       <Layout>
         <Header />
+        <div className="columns mt-4">
+          <Skills
+            className="column is-3 is-offset-1 has-border-1"
+            languagesUsed={portfolio.languages}
+          />
+        </div>
       </Layout>
       <Footer />
     </section>
